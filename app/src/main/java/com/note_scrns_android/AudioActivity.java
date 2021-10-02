dd37cf1eee648632a57a911d6fcd19a3aa7fe700
- <c0818766@mylambton.ca>
2021-10-02 13:29:56 +0530

    Start Audio functionality using MediaRecorder

--- app/src/main/java/com/note_scrns_android/AudioActivity.java
+++ app/src/main/java/com/note_scrns_android/AudioActivity.java
@@ -5,13 +5,16 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Intent;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 public class AudioActivity extends AppCompatActivity {
     TextView drawer_txt,new_note,txt_title;
@@ -19,6 +22,7 @@ public class AudioActivity extends AppCompatActivity {
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
     private boolean permissionToRecordAccepted = false;
     public String path;
+    MediaRecorder rec = new MediaRecorder();
     private final String [] permissions = {Manifest.permission.RECORD_AUDIO,Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
@@ -88,5 +92,65 @@ public class AudioActivity extends AppCompatActivity {
 
             }
         });
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    playOrStopRecording(path);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    public void start() throws IOException {
+        verifyStoragePermissions(this);
+        String file_path=getApplicationContext().getFilesDir().getPath();
+
+        File file= new File(file_path);
+
+        Long date=new Date().getTime();
+        Date current_time = new Date(Long.valueOf(date));
+
+        rec.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
+        rec.setAudioChannels(1);
+        rec.setAudioSamplingRate(8000);
+        rec.setAudioEncodingBitRate(44100);
+        rec.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        rec.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        if (!file.exists()){
+            file.mkdirs();
+        }
+
+        path =file+"/sss.3gp";
+        rec.setOutputFile(path);
+
+        try {
+            rec.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this,"Sorry! file creation failed!"+e.getMessage(),Toast.LENGTH_SHORT).show();
+            return;
+        }
+        rec.start();
+
+
+
+
+    }
+
+    public void stop() {
+        rec.stop();
+        rec.reset();
+
     }
 }
