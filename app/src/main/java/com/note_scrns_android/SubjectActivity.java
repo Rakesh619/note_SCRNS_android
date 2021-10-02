ec6cdb69fe5da377c1ae0fc5f7ad9f46e89de3ee
- <c0820794@mylambton.ca>
2021-10-02 12:32:39 +0530

    Implement and Design subject dialog screen

--- app/src/main/java/com/note_scrns_android/SubjectActivity.java
+++ app/src/main/java/com/note_scrns_android/SubjectActivity.java
@@ -1,14 +1,43 @@
 package com.note_scrns_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SubjectActivity extends AppCompatActivity {
+    TextView drawer_txt,new_note,txt_title;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject);
+        drawer_txt=(TextView)findViewById(R.id.drawer_icon);
+        new_note=(TextView)findViewById(R.id.new_note);
+        txt_title=(TextView)findViewById(R.id.txt_title);
+
+        recyclerView=(RecyclerView) findViewById(R.id.note_recycler);
+        drawer_txt.setVisibility(View.VISIBLE);
+        drawer_txt.setText("Back");
+        new_note.setText("New");
+        txt_title.setText("Subjects");
+        drawer_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+
+            }
+        });
+
+      
     }
 }
