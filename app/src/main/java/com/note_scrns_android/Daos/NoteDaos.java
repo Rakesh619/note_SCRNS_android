3d5716978ee2f2c33c459ddaa5cb9930070f7836
- <c0812619@mylambton.ca>
2021-10-03 22:09:58 +0530

    Implement interface for notes inserting updation and deletion

---
+++ app/src/main/java/com/note_scrns_android/Daos/NoteDaos.java
@@ -0,0 +1,44 @@
+package com.note_scrns_android.Daos;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+@Dao
+public interface NoteDaos {
+
+    @Query("SELECT * FROM Notes ")
+    List<Notes> getAll();
+
+    /*
+     * Insert the object in database
+     * @param note, object to be inserted
+     */
+    @Insert
+    void insert(Notes note);
+
+    /*
+     * update the object in database
+     * @param note, object to be updated
+     */
+    @Update
+    void update(Notes repos);
+
+    /*
+     * delete the object from database
+     * @param note, object to be deleted
+     */
+    @Delete
+    void delete(Notes note);
+
+    /*
+     * delete list of objects from database
+     * @param note, array of objects to be deleted
+     */
+    @Delete
+    void delete(Notes... note);      // Note... is varargs, here note is an array
+
+}
