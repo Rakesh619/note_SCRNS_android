6b5a4fac4cc46c875ac1943acf1bd3bcf6cb4499
- <maramsrijareddy@gmail.com>
2021-10-12 01:43:01 +0530

    Added Comments

--- app/src/main/java/com/note_scrns_android/DatabaseIterface/Subject_Interface.java
+++ app/src/main/java/com/note_scrns_android/DatabaseIterface/Subject_Interface.java
@@ -20,33 +20,16 @@ public interface Subject_Interface {
     @Query("SELECT * FROM SubjectPojo WHERE subject_id = :id")
     List<SubjectPojo> getSubject(int id);
 
-
-    /*
-     * Insert the object in database
-     * @param note, object to be inserted
-     */
     @Insert
     void insert(SubjectPojo subject);
 
-    /*
-     * update the object in database
-     * @param note, object to be updated
-     */
     @Update
     void update(SubjectPojo repos);
 
-    /*
-     * delete the object from database
-     * @param note, object to be deleted
-     */
     @Delete
     void delete(SubjectPojo subject);
 
-    /*
-     * delete list of objects from database
-     * @param note, array of objects to be deleted
-     */
     @Delete
-    void delete(SubjectPojo... subject);      // Note... is varargs, here note is an array
+    void delete(SubjectPojo... subject);
 
 }
